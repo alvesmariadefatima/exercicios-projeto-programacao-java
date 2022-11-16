f2ff23e7673c6c6e6d4427249908c2a2db2ded0e
Maria de Fátima Nunes Alves <94319702+alvesmariadefatima@users.noreply.github.com>
2022-11-16 11:37:18 -0300

    Delete CartaoWeb.java

--- CartaoWeb.java
+++
@@ -1,9 +0,0 @@
-package cartaoweb;
-
-public abstract class CartaoWeb {
-    String destinatario;
-    CartaoWeb(String destinatario){
-        this.destinatario = destinatario;
-    }
-    public abstract void showMessage();
-}
